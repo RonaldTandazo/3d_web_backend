8f19f117c9433d016635f3c652309dd6eb141be6
RonaldTandazo <edu09085@hotmail.com>
2025-04-15 05:33:21 +0000

    Artowrk store

--- app/graphql/GraphSchema.py
+++ app/graphql/GraphSchema.py
@@ -6,13 +6,15 @@ from app.graphql.SocialMedia.SocialMediaResolver import SocialMediaQuery
 from app.graphql.UserSocialNetwork.UserSocialNetowrkResolver import UserSocialNetworkQuery, UserSocialNetworkMutation
 from app.graphql.Category.CategoryResolver import  CategoryQuery
 from app.graphql.Publishing.PublishingResolver import PublishingQuery
+from app.graphql.Software.SoftwareResolver import SoftwareQuery
+from app.graphql.Artwork.ArtworkResolver import ArtworkMutation
 
 @strawberry.type
-class Query(AuthQuery, CountryQuery, SocialMediaQuery, UserSocialNetworkQuery, CategoryQuery, PublishingQuery):
+class Query(AuthQuery, CountryQuery, SocialMediaQuery, UserSocialNetworkQuery, CategoryQuery, PublishingQuery, SoftwareQuery):
     pass
 
 @strawberry.type
-class Mutation(AuthMutation, UserMutation, UserSocialNetworkMutation):
+class Mutation(AuthMutation, UserMutation, UserSocialNetworkMutation, ArtworkMutation):
     pass
 
 GraphSchema = strawberry.Schema(query=Query, mutation=Mutation)
